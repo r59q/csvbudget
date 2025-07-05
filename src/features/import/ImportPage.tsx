a8ae099dd2b91e05eaf41d9ea34d705d6d305335
A. Malthe Henriksen <mail@r59q.com>
2025-07-05 22:11:19 +0200

    Add TransactionsSection

--- src/features/import/ImportPage.tsx
+++ src/features/import/ImportPage.tsx
@@ -6,8 +6,9 @@ import useCSVRows from "@/hooks/CSVRows";
 import {useTransactionsContext} from "@/context/TransactionsContext";
 import CSVImport from "@/features/import/CSVImport";
 import CSVFileList from "@/features/import/CSVFileList";
-import TransactionsSection from '@/features/import/TransactionsSection';
 import ImportSteps from "@/features/import/ImportSteps";
+import TransactionsSection from "@/features/import/TransactionsSection";
+import {LinkButton} from "@/components/LinkButton";
 
 const ImportPage = () => {
     const {csvFiles, setCSVFiles, unmappedSchemas, handleSaveMapping, handleRemoveMapping} = useGlobalContext();
@@ -58,7 +59,7 @@ const ImportPage = () => {
     };
 
     return (
-        <div className="min-h-screen flex flex-col items-center">
+        <div className="min-h-screen flex flex-col items-center gap-4">
             <div className="w-2/3 mt-10 mb-6">
                 <h1 className="text-2xl font-bold mb-2">Import Your Transactions</h1>
                 <p className="text-gray-600">Easily import your CSV files, map your data, and review your transactions
@@ -70,13 +71,23 @@ const ImportPage = () => {
                 <CSVImport onFileImport={handleFileImport}/>
                 <CSVFileList csvSchemas={csvSchemas} onReset={handleRemoveMapping} onRemove={handleDelete}/>
             </div>
+
+            {/* Confirmation and next step */}
+            <div className="w-2/3 flex flex-col items-start">
+                {csvFiles.length > 0 && <>
+                    <span className="text-green-500 font-medium mb-1">CSV file(s) added successfully!</span>
+                    <span className="text-gray-200 mb-2">You can now review your transactions below or continue to the next step to filter and categorize your data.</span>
+                    <LinkButton href="/filter" className="mt-2">Continue to Filter</LinkButton>
+                </>}
+            </div>
             {/* Separation line and section for transactions table */}
             <div className="w-full flex justify-center my-10">
                 <div className="w-2/3 border-t border-gray-300 dark:border-gray-700"></div>
             </div>
-            <div className={"px-4"}>
+            <div className={"px-4 w-full"}>
                 <TransactionsSection/>
             </div>
+
         </div>
     );
 };
